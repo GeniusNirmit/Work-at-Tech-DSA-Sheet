4c490c6a38b9f225dec439d90c790f74e7d3dfc7
Nirmit Pandya <nirmitrptp@gmail.com>
2022-08-03 16:02:26 +0530

    Arrays

---
+++ Arrays/3.)Identical_Twins.cpp
@@ -0,0 +1,33 @@
+//Problem Statement:-https://workat.tech/problem-solving/practice/identical-twins
+
+int getIdenticalTwinsCount(vector<int> &arr) {
+	int maximum = arr[0];
+	int n = arr.size();
+	
+	//Found maximum of the given array
+	for(int i=0 ; i<n ; i++)
+	{
+		if(arr[i] > maximum)
+			maximum = arr[i];
+	}
+
+	//Initializes a hash table with zero 
+	int hash_table[maximum] = {0};
+	int counter = 0;
+	
+	//Iterated a for loop for creating a hash table
+	for(int i=0 ; i<n ; i++)
+	{
+		hash_table[arr[i]-1]++;
+	}
+
+	//For making pairs from the hash table
+	for(int i=0 ; i<maximum; i++)
+	{
+		if(hash_table[i]>=2)
+			counter = counter + (hash_table[i]*(hash_table[i]-1))/2;
+	}
+	return counter;
+}
+
+//According to me, this is the optimal solution of the given problem statement among various approaches.

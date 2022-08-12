d260f163df5a053a4e465c26ce81f180c327de25
Nirmit Pandya <nirmitrptp@gmail.com>
2022-08-12 23:43:56 +0530

    Two Pointers

---
+++ Two_Pointers/2.)Two_Sorted_Sum.cpp
@@ -0,0 +1,34 @@
+//Problem Statement/:-https://workat.tech/problem-solving/practice/two-sum-sorted
+
+
+//Two ways to solve a particular element
+
+
+//1.) Optimal way is with O(n) time complexity.
+bool hasTwoSumZero(vector<int> &A) {
+	int i=0,j=A.size()-1;
+	while(i<j)
+	{
+		if(-A[i]==A[j])
+			return true;
+		else if(-A[i]>A[j])
+			i++;
+		else
+			j--;
+	}
+	return false;
+}
+
+
+//2.) Another solution is with O(n^2) time complexity
+bool hasTwoSumZero(vector<int> A) {
+	for(int i=0;i<A.size();i++) 
+	{
+		for(int j=i+1;j<A.size();j++) 
+		{
+			if(A[i]+A[j]==0)
+				return true;
+		}
+	}
+	return false;
+}

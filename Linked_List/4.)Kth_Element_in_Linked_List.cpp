1973d1b16fcda17b8796c33b0fe1fb004608eb8a
Nirmit Pandya <nirmitrptp@gmail.com>
2022-08-25 18:07:46 +0530

    Linked List

---
+++ Linked_List/4.)Kth_Element_in_Linked_List.cpp
@@ -0,0 +1,26 @@
+//Problem Statement/:-https://workat.tech/problem-solving/practice/kth-element-linked-list
+
+/* This is the ListNode class definition
+
+class ListNode {
+public:
+	int data;
+	ListNode* next;
+
+	ListNode(int data) {
+		this->data = data;
+		this->next = NULL;
+	}
+};
+*/
+
+ListNode* kthElement (ListNode* head, int k) {
+    // add your logic here
+	ListNode *p=head;
+	k-=1;
+	while(k--)
+	{
+		p=p->next;
+	}
+	return p;
+}

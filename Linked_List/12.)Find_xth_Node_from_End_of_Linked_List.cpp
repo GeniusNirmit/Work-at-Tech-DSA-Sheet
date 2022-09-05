a90fb0d89e736b1cd00b32474b647b8cae81326e
Nirmit Pandya <nirmitrptp@gmail.com>
2022-09-05 22:15:03 +0530

    Linked List

---
+++ Linked_List/12.)Find_xth_Node_from_End_of_Linked_List.cpp
@@ -0,0 +1,50 @@
+//Problem Statement/:-https://workat.tech/problem-solving/practice/find-xth-node-end-linked-list
+
+/* This is the ListNode class definition
+
+class ListNode {
+public:
+	int data;
+	ListNode* next;
+
+	ListNode(int data) {
+		this->data = data;
+		this->next = NULL;
+	}
+};
+*/
+
+// Approach-1 
+
+ListNode* xthNodeFromEnd(ListNode* head, int x) {
+	// add your logic here
+	ListNode *p=head;
+	int counter=0;
+	while(p)
+	{
+		counter++;
+		p=p->next;
+	}
+	p=head;
+	counter-=x;
+	while(counter--)
+		p=p->next;
+	return p;
+}
+
+// Approach-2
+
+ListNode* xthNodeFromEnd(ListNode* head, int x) {
+	// add your logic here
+	ListNode *p=head;
+	for(int i=1;i<x;i++)
+		p=p->next;
+	if(!p->next)
+		return head;
+	while(p->next)
+	{
+		p=p->next;
+		head=head->next;
+	}
+	return head;
+}
